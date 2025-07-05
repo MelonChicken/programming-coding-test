23614ef43bc396327369f067b2abaae7f44b5c89
Junu Kim <96416870+MelonChicken@users.noreply.github.com>
2025-07-05 16:43:59 +0900

    [Bronze II] Title: 진법 변환, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        System.out.println(Integer.parseInt(arr[0],Integer.parseInt(arr[1])));
+
+    }
+}
