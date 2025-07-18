42fb3e9a37de2d188f30d644774272e01294702e
Junu Kim <96416870+MelonChicken@users.noreply.github.com>
2025-07-18 13:05:08 +0900

    [Bronze V] Title: 직사각형, Time: 100 ms, Memory: 14028 KB -BaekjoonHub

---
+++ Java/백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.valueOf(br.readLine());
+        int b = Integer.valueOf(br.readLine());
+        System.out.print(a*b);
+
+    }
+}
