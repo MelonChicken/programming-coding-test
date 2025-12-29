458a9be0b2318ed2b8c99285b3336c8b03892799
Junu Kim <96416870+MelonChicken@users.noreply.github.com>
2025-12-29 13:43:12 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 804 ms, Memory: 69120 KB -BaekjoonHub

---
+++ Java/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        for(int i = 0; i < n; i++){
+            String[] temp = br.readLine().split(" ");
+            int x = Integer.parseInt(temp[0]);
+            int y = Integer.parseInt(temp[1]);
+            arr[i][0] = x;
+            arr[i][1] = y;
+        }
+        Arrays.sort(arr, new AxisComparator());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            sb.append(arr[i][0] + " ");
+            sb.append(arr[i][1] + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static class AxisComparator implements Comparator<int[]>{
+        @Override
+        public int compare(int[] o1, int[] o2) {
+            if(o1[1] != o2[1]){
+                return o1[1] - o2[1];
+            }
+
+            if(o1[0] != o2[0]){
+                return o1[0] - o2[0];
+            }
+
+            return 0;
+        }
+
+    }
+}
