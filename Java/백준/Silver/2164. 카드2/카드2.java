c59af2aaf7053fa18af10d3be22a3adcfeaa911e
Junu Kim <96416870+MelonChicken@users.noreply.github.com>
2026-01-12 12:18:19 +0900

    [Silver IV] Title: 카드2, Time: 228 ms, Memory: 32088 KB -BaekjoonHub

---
+++ Java/백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+import java.util.Queue;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i = 1; i <= n; i++){
+            q.add(i);
+        }
+        while(q.size() != 1){
+            q.poll();
+            q.add(q.poll());
+        }
+        System.out.println(q.poll());
+    }
+}
