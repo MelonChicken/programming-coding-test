32d378e90a82fcb0ca83626932f7868177f0545c
Junu Kim <96416870+MelonChicken@users.noreply.github.com>
2026-01-14 09:57:21 +0900

    [Bronze IV] Title: 녹색거탑, Time: 164 ms, Memory: 17604 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(Math.round(Math.pow(2,n)));
+
+    }
+}
